7e0d027fb0dbc410d2f66b9875807b3616958511
Enio N. de Souza <enionsouza@gmail.com>
2021-11-17 18:28:17 -0400

    fix: Fix the login procedure for the user, with proper authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
   has_many :events, foreign_key: :creator_id, dependent: :destroy
   has_many :attendances, foreign_key: :attendee_id, dependent: :destroy
   has_many :attended_events, through: :attendances, source: :event
+
+  attr_accessor :token
+
+  def on_jwt_dispatch(token, _)
+    @token = token.to_s
+  end
 end
