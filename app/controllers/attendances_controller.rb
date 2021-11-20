49abd66310a98944a02ca779aca53a5c2f5f50c2
aristides1000 <aristides1000@gmail.com>
2021-11-20 17:53:29 -0400

    Finish Reserver Event

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,19 @@
 class AttendancesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @attendance = current_user.attendances.build(attendance_params)
+
+    if @attendance.save
+      render json: @attendance, status: :created, location: @attendance
+    else
+      render json: @attendance.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def attendance_params
+    params.permit(:event_id)
+  end
 end
